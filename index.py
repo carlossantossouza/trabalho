c12dff0263e272191b17521d5c5b52d568b503a7
Carlos Dos Santos Souza <carlossantosouza@gmail.com>
2025-03-30 13:57:15 -0300

    divisao

--- index.py
+++ index.py
@@ -6,5 +6,17 @@ def subtrair(a, b):
     return a - b
 
 
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    if b == 0:
+        return "Erro: Divisão por zero"
+    return a / b
+
+
 print("Resultado da soma:", somar(5, 3))
 print("Resultado da subtração:", subtrair(5, 3))
+print("Resultado da multiplicação:", multiplicar(5, 3))
+print("Resultado da divisão:", dividir(5, 0))
